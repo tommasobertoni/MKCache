159d4154a63344732b9d0afb7a9bb6ec04203e2f
Tommaso Bertoni <TBertoni@artiso.com>
2021-03-22 09:43:47 +0100

    Introduces MKCache.Tests.

---
+++ tests/MKCache.Tests/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MKCache.Tests
+{
+    public class Item
+    {
+        public Item()
+            : this(name: Guid.NewGuid().ToString("n"))
+        {
+        }
+
+        public Item(string name)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+            Count = DateTime.UtcNow.Second;
+        }
+
+        public string Id { get; }
+
+        public string Name { get; }
+
+        public int Count { get; }
+    }
+}
